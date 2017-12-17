7a1649567790b2e9b880bf4fe90e3be90b2d0970
Unknown <td736@york.ac.uk>
2017-12-17 12:56:20 +0000

    Created scrape() function to call for multiple url's.

--- Scraper.py
+++ Scraper.py
@@ -4,31 +4,38 @@ import urllib.request
 - Replace hardcoded parts with variables
 - Tidy up.
 '''
-sauce = urllib.request.urlopen('http://quotes.wsj.com/CH/XVTX/NOVN/research-ratings').read()
-
-soup = bs.BeautifulSoup(sauce, 'lxml')
-
-out = []
-for s in soup.find_all('span'):
-    out.append(str(s))
-
-data_loc = {}
-
-for item in out:
-    if 'Average' in item:
-        data_loc['Avg'] = out.index(item) + 1
-
-    if 'Current Price' in item:
-        data_loc['Crn'] = out.index(item) + 1
 
 
-def remove_tags(input_str):
-    input_str = input_str.replace('<span class="data_data"><sup> </sup>', '')
-    input_str = input_str.replace('</span>', '')
-    return input_str
+def scrape(url = 'http://quotes.wsj.com/CH/XVTX/NOVN/research-ratings'):
 
-data_out = {}
+    #sauce = urllib.request.urlopen('http://quotes.wsj.com/CH/XVTX/NOVN/research-ratings').read()
+    sauce = urllib.request.urlopen(url).read()
 
-data_out['Avg'] = float(remove_tags(out[data_loc['Avg']]))
-data_out['Crn'] = float(remove_tags(out[data_loc['Crn']]))
+    soup = bs.BeautifulSoup(sauce, 'lxml')
+
+    out = []
+    for s in soup.find_all('span'):
+        out.append(str(s))
+
+    data_loc = {}
+
+    for item in out:
+        if 'Average' in item:
+            data_loc['Avg'] = out.index(item) + 1
+
+        if 'Current Price' in item:
+            data_loc['Crn'] = out.index(item) + 1
+
+
+    def remove_tags(input_str):
+        input_str = input_str.replace('<span class="data_data"><sup> </sup>', '')
+        input_str = input_str.replace('</span>', '')
+        return input_str
+
+    data_out = {}
+
+    data_out['Avg'] = float(remove_tags(out[data_loc['Avg']]))
+    data_out['Crn'] = float(remove_tags(out[data_loc['Crn']]))
+
+    return data_out
 
