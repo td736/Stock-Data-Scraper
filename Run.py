0f7d0c262b3ebc82301d2f9f3742f4e3f2aad5f1
Unknown <td736@york.ac.uk>
2017-12-18 15:35:14 +0000

    New run script.

---
+++ Run.py
@@ -0,0 +1,23 @@
+from Page import Page
+from Scraper import Scraper
+
+import json
+import datetime
+
+file = open('tags.txt', 'r')
+tags = file.read()
+file.close()
+
+tags_list = tags.split('\n')
+
+pages = []
+
+for tag in tags_list:
+    pages.append(Page(tag))
+
+scraper = Scraper(pages)
+scraper.scrape()
+
+with open('%s-%s.txt'%(datetime.datetime.now().day,datetime.datetime.now().month), 'w') as out:
+    out.write(json.dumps(scraper.output))
+
